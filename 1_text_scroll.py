3aaa0bae96e27722ebb44dd2b019cc444a939f84
lindaktong <42878194+lindaktong@users.noreply.github.com>
2019-06-21 09:18:58 +0700

    Update 1_text_scroll.py

--- 1_text_scroll.py
+++ 1_text_scroll.py
@@ -1,3 +1,3 @@
 from sense_hat import SenseHat
 sense = SenseHat()
-sense.show_message("Khoa and Travis are stinky. Jaewan needs a bath!")
+sense.show_message("Khoa and Travis are stinky. Graeme needs a bath!")
